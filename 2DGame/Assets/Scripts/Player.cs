332fe8729b7f3c164a763e57d919d66bc7c6d6e8
eungyu kim <86348187+eungyukm@users.noreply.github.com>
2024-10-08 23:00:21 +0900

    플레이어 움직임  구현

--- 2DGame/Assets/Scripts/Player.cs
+++ 2DGame/Assets/Scripts/Player.cs
@@ -3,5 +3,25 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 4;
     public InputAction moveAction;
+    
+    private void Start()
+    {
+        moveAction.Enable();
+    }
+    
+    private void Update()
+    {
+        Vector2 input = moveAction.ReadValue<Vector2>();
+        Debug.Log(input);
+        
+        if (input.x != 0 || input.y != 0)
+        {
+            Vector3 movement = new Vector3(input.x, input.y, 0);
+            Vector3 direction = movement.normalized;
+            Debug.Log($"direction: {direction}");
+            transform.position += direction * Time.deltaTime * speed;
+        }
+    }
 }
